a43d7300287af51950cc2b54fb46c1f021c8b6c1
julianstoerig <julian.stoerig@gmail.com>
2024-04-04 19:23:17 +0200

    Refactor xorswift

--- xorswift.cpp
+++ xorswift.cpp
@@ -3,53 +3,43 @@ Constant time and low memory non-cryptographic pseudo-random-number generation.
 */
 
 #include <iostream>
-#include <chrono>
-#include <thread>
 #include <climits>
 
 using namespace::std;
 
 
-float normalise(int raw) {
+struct State {
+    int state;
+};
+
+
+static constexpr int U = 13;
+static constexpr int V = 17;
+static constexpr int W = 5;
+static constexpr State STATE = State {
+    .state = 314159265
+};
+
+
+float normalise(const int& raw) {
+    /*take any integer between [INT_MIN; INT_MAX] and map them to [0; 1].*/
     int preprocessed = raw < 0 ? raw : -raw;
     return (double)(preprocessed - INT_MIN)/(double)INT_MAX;
 }
 
 
-int xorshift_parametarised(int state, const int& u, const int& v, const int& w) {
+State progress_state(const State& old_state, const int& u, const int& v, const int& w) {
+    int result = old_state.state;
 
-    state ^= (state << u);
-    state ^= (state >> v);
-    state ^= (state << w);
+    result ^= (result << u);
+    result ^= (result >> v);
+    result ^= (result << w);
 
-    return state;
+    return State { .state = result };
 }
 
-
-
-
-class Rng {
-public:
-    float next();
-private:
-    int xorshift_parametarised(int state, const int& u, const int& v, const int& w);
-    void xorshift();
-    void initialise();
-    float random_number;
-    int state;
-    const int u = 13;
-    const int v = 17;
-    const int w = 5;
-};
-
-
-
-float Rng::next() {
-    xorshift();
-    return normalise(state);
-}
-
-
-void Rng::initialise() {
-    state = 314159265;
+pair<float, State> xorswift(const State& old_state) {
+    /*Takes in the previous state and returns a std::pair of a random float in range [0; 1] and a new State.*/
+    const State new_state = progress_state(old_state, U, V, W);
+    return pair(normalise(new_state.state), new_state);
 }
