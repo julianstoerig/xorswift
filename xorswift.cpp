3f2960dbba565fa1a845e8cfcc6c09fe4e29daf3
julianstoerig <julian.stoerig@gmail.com>
2024-04-06 20:21:02 +0200

    add doc comment

--- xorswift.cpp
+++ xorswift.cpp
@@ -17,12 +17,14 @@ static constexpr int U = 13;
 static constexpr int V = 17;
 static constexpr int W = 5;
 static constexpr State STATE = State {
-    .state = 314159265
+    .state = 314159265 // magic number, ask wikipedia
 };
 
 
+/// @brief take any integer between [INT_MIN; INT_MAX] and map them to ]0; 1]
+/// @param the value to normalise
+/// @return the normalised value
 float normalise(const int& raw) {
-    /*take any integer between [INT_MIN; INT_MAX] and map them to ]0; 1].*/
     int raw_positive = raw < 0 ? raw : -raw;
     float normalised = (double)(raw_positive - INT_MIN)/(double)INT_MAX;
     return normalised;
@@ -38,6 +40,7 @@ State progress_state(const State& old_state, const int& u, const int& v, const i
 
     return State { .state = result };
 }
+
 
 pair<float, State> xorswift(const State& old_state) {
     /*Takes in the previous state and returns a std::pair of a random float in range [0; 1] and a new State.*/
